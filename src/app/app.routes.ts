64d0d8e39f9c2f4e3305abb1a9a5594c7fa877b4
Web-Who <webwho.br@gmail.com>
2025-09-21 20:35:06 -0300

    feat: add Limits Explorer component and integrate with routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
 import { LinearCheckerComponent } from './components/linear-checker/linear-checker.component';
+import { LimitsExplorerComponent } from './components/limits-explorer/limits-explorer.component';
 
 export const routes: Routes = [
-    {
-        path:'',component:LinearCheckerComponent
-    }
+  {
+    path: '',
+    component: LinearCheckerComponent,
+  },
+  {
+    path: 'limites',
+    component: LimitsExplorerComponent,
+  },
 ];
